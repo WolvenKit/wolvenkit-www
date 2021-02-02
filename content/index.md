cc1e5498cd1e5e719df98f4e2d310cf6ccb0b67e
Soulweaver <aspifie@gmail.com>
2021-02-02 11:44:18 +1000

    Add more landing page design changes

--- content/index.md
+++ content/index.md
@@ -1,14 +1,19 @@
 ---
-title: RED Modding (can be edited in yaml/md)
+title: RED Modding Tools
 tools:
-  - name: 'WolvenKit'
-    test: 1
-  - name: 'CET'
-    test: 2
+  - name: WolvenKit
+    description: Mod editor/creator for RED Engine games. The point is to have an all in one tool for creating mods for the games made with the engine.
+    backgroundImage: https://cdn.discordapp.com/attachments/717720094196760760/804294114366717982/unknown.png
+    featureImage: https://cdn.discordapp.com/attachments/803648048018096170/803926577842618408/unknown.png
+    pageLink: /projects/wolvenkit
+  - name: CET
+    description: Performance boost, bug fixes and hacks for fun for Cyberpunk 2077.
+    backgroundImage: https://staticdelivery.nexusmods.com/mods/3333/images/808/808-1611363787-788075230.jpeg
+    featureImage: https://staticdelivery.nexusmods.com/mods/3333/images/1136/1136-1611620025-968345420.jpeg
+    pageLink: /projects/cet
+  - name: CyberCAT
+    description: CyberPunk 2077 Customization Assistant Tool. Work in progress savegame editor.
+    backgroundImage: https://steamuserimages-a.akamaihd.net/ugc/1675869325571392320/C0F8B7D7D378957366799DB34666969C7BE48D39/
+    featureImage: https://cdn.discordapp.com/attachments/789565732726767636/796036604682436678/unknown.png
+    pageLink: /projects/cybercat
 ---
-
-# Content title
-
-Users can write page data using Markdown
-
-```With codeblocks if needed```
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,80 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
+  <div class="index">
+    <div class="index__imageContainer">
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <img
+          :key="currentTool"
+          :src="page.tools[currentTool].backgroundImage"
+        >
+      </transition>
+    </div>
+    <div class="index__mainContainer">
+      <span class="index__spacer" />
+      <h1 class="index__title">
         {{ page.title }}
       </h1>
-      <div class="links">
-        <p
-          v-for="tool in page.tools"
-          :key="tool"
+
+      <ul class="index__toolNav">
+        <li
+          v-for="(tool, index) in page.tools"
+          :key="index"
+          class="index__toolNavItem"
+          :class="{ 'index__toolNavItem--active': currentTool === index }"
+          @click="changeTool(index)"
         >
-          {{ tool }}
-        </p>
+          {{ tool.name }}
+        </li>
+      </ul>
+
+      <p class="index__toolDescription">
+        {{ page.tools[currentTool].description }}
+      </p>
+
+      <div class="index__toolLinks">
+        <router-link
+          :to="page.tools[currentTool].pageLink"
+          class="index__toolLink"
+        >
+          Get Started
+        </router-link>
+        <a
+          class="index__toolLink"
+          href="#"
+        >
+          <GithubIcon />
+          Github
+        </a>
       </div>
-      <nuxt-content :document="page" />
+
+      <div class="index__toolPreview">
+        <transition
+          name="fade"
+          mode="out-in"
+        >
+          <img
+            :key="currentTool"
+            :src="page.tools[currentTool].featureImage"
+          >
+        </transition>
+      </div>
+    </div>
+    <div>
+      <h1>yeeeeet</h1>
     </div>
   </div>
 </template>
 
 <script>
+import GithubIcon from 'vue-material-design-icons/Github.vue'
+
 export default {
+  components: {
+    GithubIcon
+  },
+
   async asyncData ({ $content, error }) {
     const page = await $content('index')
       .fetch()
@@ -26,50 +82,181 @@ export default {
         error({ statusCode: 404, message: 'Page not found' })
       })
 
+    const currentTool = 0
+
     return {
-      page
+      page,
+      currentTool
+    }
+  },
+
+  methods: {
+    changeTool (index) {
+      this.currentTool = index
     }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
+<style lang="scss" scoped>
+.index {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+
+  &__mainContainer {
+    position: relative;
+    height: 115vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: var(--max-width-lg);
+    margin: 0 auto;
+    padding: 0 4vmin;
+
+    &::before {
+      content: '';
+      height: 8vmin;
+    }
+  }
+
+  &__imageContainer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 115vh;
+    z-index: -1;
+    overflow: hidden;
+
+    > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+      filter: blur(5px);
+      transform: scale(1.05);
+      opacity: 0.3;
+    }
+  }
+
+  &__spacer {
+    margin-top: 25vmin;
+  }
+
+  &__title {
+    font-weight: 700;
+    font-size: 4em;
+    text-align: center;
+  }
+
+  &__toolNav {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    padding: 0;
+    margin: 1em 0;
+  }
+
+  &__toolNavItem {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:not(:last-of-type) {
+      margin-right: 0.5em;
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+      bottom: 0.25em;
+      left: 50%;
+      width: 90%;
+      transform: translate(-50%, 500%);
+      height: 2px;
+      background: var(--color-text);
+      opacity: 0;
+      transition: all 0.2s ease-in-out;
+    }
+
+    &::after {
+      content: attr(data-text);
+      content: attr(data-text) / "";
+      height: 0;
+      visibility: hidden;
+      overflow: hidden;
+      user-select: none;
+      pointer-events: none;
+      font-weight: 600;
+    }
+
+    &--active {
+      font-weight: 600;
+
+      &::before {
+        transform: translate(-50%, 100%);
+        opacity: 1;
+      }
+    }
+  }
+
+  &__toolLinks {
+    display: flex;
+  }
+
+  &__toolLink {
+    height: 3em;
+    display: flex;
+    align-items: center;
+    background: var(--color-primary);
+    color: var(--color-text);
+    padding: 0 2em;
+    margin-top: 3vh;
+    border-radius: 0.25em;
+    font-weight: 600;
+    text-decoration: none;
+
+    &:not(:last-of-type) {
+      margin-right: 1em;
+    }
+
+    > .github-icon {
+      height: 60%;
+      width: 100%;
+      margin-right: 0.5em;
+
+      > svg {
+        height: 100%;
+        width: 100%;
+      }
+    }
+  }
+
+  &__toolPreview {
+    width: 100%;
+    height: 100%;
+    margin-top: 3vmin;
+    border-radius: 0.5em 0.5em 0 0;
+    overflow: hidden;
+
+    > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+  }
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.fade-enter,
+.fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0 !important;
 }
 </style>
