5f8a07a8d838628fbcc87a6d5850d63b9a02ee51
Moonded <moondedow@gmail.com>
2021-02-10 01:03:18 +0100

    Update nuxt.config.js
    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'WolvenKit',
+    title: 'Red Modding',
     htmlAttrs: {
       lang: 'en'
     },
