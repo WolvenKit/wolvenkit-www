7a021f4e47bdfe958c6ccffbe9357563de7acb9d
phikko <skeezze@gmail.com>
2021-02-12 14:57:04 +0100

    feat: added animated Wumpus

---
+++ pages/index/-components/Wumpus.vue
@@ -0,0 +1,31 @@
+<template>
+  <canvas ref="riveWumpus" class="wumpus-canvas" width="300px" height="300px" />
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      script: [
+        { hid: 'rive', src: 'https://unpkg.com/rive-js/dist/rive.js', defer: false }
+      ]
+    }
+  },
+  mounted () {
+    // eslint-disable-next-line no-undef,no-new
+    new RiveAnimation({
+      src: '/rive/wumpus.riv',
+      canvas: this.$refs.riveWumpus,
+      autoplay: true
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+.wumpus-canvas {
+  @media (max-width: 660px) {
+    display: none;
+  }
+}
+</style>
