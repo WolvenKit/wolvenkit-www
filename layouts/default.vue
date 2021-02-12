ab411a931eb7ca9207ae29ad73274347bae1c76b
Soulweaver <aspifie@gmail.com>
2021-02-13 00:10:24 +1000

    refactor: remove old css from layout - adds sticky footer, plenty of content per page but... you never know

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="layout">
     <Navbar />
-    <Nuxt />
+    <Nuxt class="layout__content" />
     <PageFooter />
   </div>
 </template>
@@ -33,33 +33,16 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
+</style>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+<style lang="scss" scoped>
+.layout {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  &__content {
+    flex: 1;
+  }
 }
 </style>
