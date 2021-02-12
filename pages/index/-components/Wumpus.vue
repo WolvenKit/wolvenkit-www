376ed13c817cc82d9eb9f2135c6b6ae475285cb5
phikko <skeezze@gmail.com>
2021-02-12 16:09:19 +0100

    fix: RiveAnimation is now initiated after the script loads

--- pages/index/-components/Wumpus.vue
+++ pages/index/-components/Wumpus.vue
@@ -7,23 +7,28 @@ export default {
   head () {
     return {
       script: [
-        { hid: 'rive', src: 'https://unpkg.com/rive-js/dist/rive.js', defer: false }
+        {
+          hid: 'rive',
+          src: 'https://unpkg.com/rive-js/dist/rive.js',
+          callback: () => {
+            // eslint-disable-next-line no-new, no-undef
+            new RiveAnimation({
+              src: '/rive/wumpus.riv',
+              canvas: this.$refs.riveWumpus,
+              autoplay: true
+            })
+          }
+        }
       ]
     }
-  },
-  mounted () {
-    // eslint-disable-next-line no-undef,no-new
-    new RiveAnimation({
-      src: '/rive/wumpus.riv',
-      canvas: this.$refs.riveWumpus,
-      autoplay: true
-    })
   }
 }
 </script>
 
 <style lang="scss">
 .wumpus-canvas {
+  transform: rotate(-3deg);
+
   @media (max-width: 660px) {
     display: none;
   }
