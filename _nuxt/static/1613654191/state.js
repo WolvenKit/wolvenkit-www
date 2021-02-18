d9fd8a2be611e677f6c294e34ef38119ba9d4135
Moonded <Moonded@users.noreply.github.com>
2021-02-18 13:24:05 +0000

    deploy: 9d7915f20a3708ea4e92558db24b0e11350e5351

--- _nuxt/static/1613654191/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613654191",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"41b8876a"}}}}(null,"\u002F"));
