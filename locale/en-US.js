72327cb79eae02af2792a7e5255d3c37e4b17c30
zSoulweaver <aspifie@gmail.com>
2021-03-12 20:10:04 +1000

    feat: add patreon links to index and nav (#40)
    * feat: add patreon section to index
    
    * feat: add Patreon logo to navbar

--- locale/en-US.js
+++ locale/en-US.js
@@ -38,6 +38,9 @@ export default {
     contribute: 'Contribute',
     contributeParagraph: 'Submit your PRs to the various projects we have ongoing in our repos, we are always keen on welcoming new developers to help and contribute to the projects.',
     contributeRepoButton: 'View Repositories',
+    patreon: 'Support the Developers',
+    patreonParagraph: 'The Red Modding developers contribute their free time creating various free open-source software to mod RedEngine games. Our patrons enable us to contribute more time, and keep us motivated to continue to help the community.',
+    patreonButton: 'Become a Patron',
     tools: {
       wolvenkitDescription: 'Mod editor/creator for RED Engine games. The point is to have an all in one tool for creating mods for the games made with the engine.',
       cetDescription: 'Performance boost, bug fixes and hacks for fun for Cyberpunk 2077.',
